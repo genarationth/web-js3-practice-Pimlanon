26de49a248eac02f94f49815a278c528bfc29014
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-13 09:28:33 +0000

    completed task2

--- function.js
+++ function.js
@@ -2,11 +2,15 @@
 const add = (a,b) => console.log(a + b);
 add(10,5);
 
-const subtract = (a,b) => console.log(a-b);
+const subtract = (a,b) => console.log(a - b);
 subtract(10,5);
 
-const multiply = (a,b) => console.log(a*b);
+const multiply = (a,b) => console.log(a * b);
 multiply(10,5);
 
-const divide = (a,b) => console.log(a/b);
+const divide = (a,b) => console.log(a / b);
 divide(10,5);
+
+//task2
+const myInfo = (firstName, location, hobby) => console.log("Hi, my name is " + firstName + ". I live in " + location + " and I enjoy "+ hobby);
+myInfo('Non', 'Bangkok', 'Sleeping');
