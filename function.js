7d44fa61a900222b7241dde65030d27d019ae8f8
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-13 09:21:18 +0000

    completed task1

---
+++ function.js
@@ -0,0 +1,12 @@
+// task1
+const add = (a,b) => console.log(a + b);
+add(10,5);
+
+const subtract = (a,b) => console.log(a-b);
+subtract(10,5);
+
+const multiply = (a,b) => console.log(a*b);
+multiply(10,5);
+
+const divide = (a,b) => console.log(a/b);
+divide(10,5);
